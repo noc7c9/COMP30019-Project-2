393dd7ac6a4d0fb13cfeb61553fb890710d3d4d3
mariam shahid <mariam shahid>
2016-09-26 17:04:41 +1000

    Added phong illumination to the floor

---
+++ Assets/Game/Floor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Floor : MonoBehaviour {
+    public GameObject[] cores;
+
+    public readonly int MAX_CORES = 2;
+
+	// Use this for initialization
+	void Start () {
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.SetFloat("_AmbientCoeff", 1.0f);
+        renderer.material.SetFloat("_DiffuseCoeff", 1.0f);
+        renderer.material.SetFloat("_SpecularCoeff", 20.0f);
+        renderer.material.SetFloat("_SpecularPower", 200.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3[] corePositions = new Vector3[this.cores.Length] ;
+        Color[] coreColours = new Color[this.cores.Length];
+
+        this.cores = GameObject.FindGameObjectsWithTag("Core");
+
+        if (cores.Length > MAX_CORES) {
+            Debug.Log("More cores than the maximum allowable limit");
+        } else {
+            GetComponent<Renderer>().material.SetInt("_NumCores", this.cores.Length);
+            //Debug.Log("num cores = " + this.cores.Length);
+
+            for (int i = 0; i < cores.Length; i++) {
+                corePositions[i] = cores[i].transform.position;
+                coreColours[i] = cores[i].GetComponent<Renderer>().material.color;
+            }
+        }
+        
+
+        PassArrayToShader.Vector3(GetComponent<Renderer>().material, "_CorePositions", corePositions);
+        PassArrayToShader.Color(GetComponent<Renderer>().material, "_CoreColours", coreColours);
+    }
+}
