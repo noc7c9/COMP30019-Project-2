e1f69884915844c9f04222740668d4cf6d3d2464
Athir Saleem <noc7c9@gmail.com>
2016-10-17 20:51:56 +1100

    health bars!

--- Assets/Game Objects/HealthPoints.cs
+++ Assets/Game Objects/HealthPoints.cs
@@ -3,15 +3,32 @@ using System.Collections;
 
 public class HealthPoints : MonoBehaviour {
 
-    BuildPointsManager buildPointsManager;
+    public Texture2D texture;
+    public float healthBarHeight = 3;
+    public float healthBarWidth = 30;
+    public float healthBarYOffset = -5;
+    public float healthBarXOffset = 0;
 
     public int buildPointWorth = 1;
 
     public int healthPoints = 10;
     public int maxHealthPoints = 10;
 
+    BuildPointsManager buildPointsManager;
+
     bool displayHP = true;
     ParticleSystem ps;
+
+	GUIStyle style_;
+	GUIStyle style {
+		get {
+			if (style_ == null) {
+				style_ = new GUIStyle(GUI.skin.GetStyle("label"));
+				style_.normal.background = texture;
+			}
+			return style_;
+		}
+	}
 
     void Start() {
         // on destruction we will increment the opposing sides build points
@@ -39,8 +56,12 @@ public class HealthPoints : MonoBehaviour {
         if (displayHP) {
             Vector2 center = Camera.main.WorldToScreenPoint(transform.position);
             center.y = Screen.height - center.y;
-            float width = 40 * healthPoints / (float)maxHealthPoints;
-            GUI.Label(new Rect(center.x - 20, center.y - 40, width, 20), "", "box");
+            float width = healthBarWidth * healthPoints / (float)maxHealthPoints;
+            Rect rect = new Rect(
+                center.x - healthBarWidth / 2 + healthBarXOffset,
+                center.y + healthBarYOffset,
+                width, healthBarHeight);
+            GUI.Label(rect, "", style);
         }
         
     }
